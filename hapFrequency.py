91aeb0abf841e9eacff7334de3920147972bdf85
Tyler K. Chafin <tkchafin@uark.edu>
2019-07-05 12:20:11 -0500

    added functions to treat as diplotypes, and re-code haps in short form

--- hapFrequency.py
+++ hapFrequency.py
@@ -16,9 +16,23 @@ def main():
 		pop_assign = dict()
 		seqs = dict()
 		#print('Reading alignment from FASTA...')
+
+		#check if there is a certain way user wants haps to be coded
+		coding=dict()
+		if params.code_table:
+			coding=parsePopmap(params.code_table)
+
 		for f in read_fasta(params.fasta):
-			seqs[f[0]] = f[1]
-			
+			if params.code_table and coding[f[1]]:
+				seqs[f[0]] = coding[f[1]]
+			else:
+				seqs[f[0]] = f[1]
+
+		if params.dip:
+			#parse fasta list to conjoin haplotypes
+			seqs = conjoinDiplotypes(seqs)
+
+
 		#parse popmap file for dictionary of sample assignments
 		if params.popmap:
 			#print("Parsing popmap file...")
@@ -29,11 +43,13 @@ def main():
 		# 		line = key + "\t" + val + "\n"
 		# 		f.write(line)
 		# 	f.close()
-			
-		#Make dict of lists representing populations 
+
+		#Make dict of lists representing populations
 		pops = dict()
 		pops["TOTAL"]=(list(seqs.values()))
+
 		if params.popmap:
+			allpops=list()
 			for samp in seqs:
 				if samp in pop_assign:
 					if pop_assign[samp] in pops:
@@ -43,17 +59,20 @@ def main():
 						t.append(seqs[samp])
 						#print("adding", pop_assign[samp],"to dict")
 						pops[pop_assign[samp]]=t
+					allpops.append(seqs[samp])
 				else:
 					print("Warning: Sample %s isn't in popmap."%samp)
-					
+			pops["ALLPOPS"]=allpops
+
 		#Grab table contents
 		hapTotals = dict()
 		hapCounts = dict()
 		for hap in pops["TOTAL"]:
 			hapTotals[hap] = dict()
 			hapCounts[hap] = dict()
-		#Now, parse pops to get frequencies 
+		#Now, parse pops to get frequencies
 		for pop in pops:
+			print("pop")
 			counts = dict()
 			total = 0
 			for hap in pops[pop]:
@@ -69,18 +88,22 @@ def main():
 				hapTotals[key][pop] = f
 				hapCounts[key][pop] = counts[key]
 			print()
-			
-		#Write table 
+
+
+		#Write table
 		with open(params.out, 'w') as fh:
 			try:
 				popOrder = pops.keys()
 				header = "Haplotype"
+				if params.dip:
+					header="Diplotype"
+
 				for p in popOrder:
 					header = header + "\t" + str(p)
 				header = header + "\n"
 				fh.write(header)
-				
-				#Print haplotype line 
+
+				#Print haplotype line
 				for hap in hapTotals:
 					hapLine = str(hap)
 					for p in popOrder:
@@ -90,23 +113,26 @@ def main():
 							hapLine = hapLine + "\t" + str(0.0000)
 					hapLine = hapLine + "\n"
 					fh.write(hapLine)
-			except IOError: 
+			except IOError:
 				print("Could not read file ",fh)
 				sys.exit(1)
 			finally:
 				fh.close()
-				
-			#write counts table 
+
+			#write counts table
 			with open(params.out2, 'w') as fh:
 				try:
 					popOrder = pops.keys()
 					header = "Haplotype"
+					if params.dip:
+						header="Diplotype"
+
 					for p in popOrder:
 						header = header + "\t" + str(p)
 					header = header + "\n"
 					fh.write(header)
-					
-					#Print haplotype line 
+
+					#Print haplotype line
 					for hap in hapTotals:
 						hapLine = str(hap)
 						for p in popOrder:
@@ -116,15 +142,12 @@ def main():
 								hapLine = hapLine + "\t" + str(0)
 						hapLine = hapLine + "\n"
 						fh.write(hapLine)
-				except IOError: 
+				except IOError:
 					print("Could not read file ",fh)
 					sys.exit(1)
 				finally:
 					fh.close()
 
-
-
-		
 	else:
 		print("No input provided")
 		sys.exit(1)
@@ -160,7 +183,7 @@ def read_fasta(fas):
 				#Iyield last sequence, if it has both a header and sequence
 				if contig and seq:
 					yield([contig,seq])
-			except IOError: 
+			except IOError:
 				print("Could not read file ",fas)
 				sys.exit(1)
 			finally:
@@ -168,6 +191,23 @@ def read_fasta(fas):
 	else:
 		raise FileNotFoundError("File %s not found!"%fas)
 
+
+#function joins diplotypes from a sequence list where paired haps end with _A and _B
+#note: samples will retain the _A name, to ensure they match the popmap
+def conjoinDiplotypes(haps):
+	dips=dict()
+	for name, seq in haps.items():
+		if "_A" in name:
+			paired=name.replace("_A", "_B")
+			if haps[paired]:
+				print(name, "=", seq,"/",haps[paired])
+				if seq < haps[paired]:
+					dips[name] = seq + "/" + haps[paired]
+				else:
+					dips[name] = haps[paired] + "/" + seq
+			else:
+				print("Skipping sample: Haplotype",name,"has no pair! Should be:",paired)
+	return(dips)
 
 #Function to check that list of sample names and popmap entries match
 def validatePopmap(samples, popmap):
@@ -180,7 +220,7 @@ def validatePopmap(samples, popmap):
 		if key not in samples:
 			print("Warning: Sample %s found in popmap has no data!"%key)
 
-#function reads a tab-delimited popmap file and return dictionary of assignments 
+#function reads a tab-delimited popmap file and return dictionary of assignments
 def parsePopmap(popmap):
 
 	ret = dict()
@@ -198,7 +238,7 @@ def parsePopmap(popmap):
 						ret[stuff[0]] = stuff[1]
 					#print(line)
 				return(ret)
-			except IOError: 
+			except IOError:
 				print("Could not read file ",popmap)
 				sys.exit(1)
 			finally:
@@ -212,8 +252,8 @@ class parseArgs():
 	def __init__(self):
 		#Define options
 		try:
-			options, remainder = getopt.getopt(sys.argv[1:], 'p:o:hf:d', \
-			["popmap=","out=","help","fasta=","dip"])
+			options, remainder = getopt.getopt(sys.argv[1:], 'p:o:hf:dc:', \
+			["help"])
 		except getopt.GetoptError as err:
 			print(err)
 			self.display_help("\nExiting because getopt returned non-zero exit status.")
@@ -224,10 +264,11 @@ class parseArgs():
 		self.fasta=None
 		self.dip=False
 		self.out2=None
+		self.code_table=None
 
 		#First pass to see if help menu was called
 		for o, a in options:
-			if o in ("-h", "-help", "--help"):
+			if o =="-h" or o=="--help":
 				self.display_help("Exiting because help menu was called.")
 
 		#Second pass to set all args.
@@ -236,16 +277,18 @@ class parseArgs():
 			arg = arg.strip()
 			opt = opt.replace("-","")
 			#print(opt,arg)
-			if opt in ('p', 'popmap'):
+			if opt == "p":
 				self.popmap= arg
-			elif opt in ('h', 'help'):
+			elif opt == "h":
 				pass
-			elif opt in ('o','out'):
+			elif opt == "o":
 				self.out = arg
-			elif opt in ("f","fasta"):
+			elif opt == "f":
 				self.fasta = arg
-			elif opt in ("d","dip"):
+			elif opt == "d":
 				self.dip=True
+			elif opt == "c":
+				self.code_table = arg
 			else:
 				assert False, "Unhandled option %r"%opt
 
@@ -254,11 +297,12 @@ class parseArgs():
 			self.display_help("Error: Missing required input file (-f,--fasta).")
 
 		if self.out:
-			self.out = self.out + "_freq.tsv"
 			self.out2 = self.out + "_counts.tsv"
+			self.out = self.out + "_freq.tsv"
 		else:
-			self.out = "out_freq.tsv"
 			self.out2 = "out_counts.tsv"
+			self.out = "out_freq.tsv"
+
 
 
 	def display_help(self, message=None):
@@ -272,10 +316,15 @@ class parseArgs():
 
 		print("""
 	Arguments:
-		-f,--fasta	: Path to FASTA file. If diplotypes, separate with _A and _B
-		-o,--out	: Prefix for output file <default = ./out>
-		-p,--popmap	: Path to tab-delimited population map, if you want to group
-		-h,--help	: Displays help menu
+		-f	: Path to FASTA file. If diplotypes, separate with _A and _B
+		-o	: Prefix for output file <default = ./out>
+		-p	: Path to tab-delimited population map, if you want to group
+			  --NOTE: Output table will include column "TOTAL" for all samples (inc. not in popmap)
+			          and a column "ALLPOPS" which will include only samples from the popmap.
+			          This is intended to make it easy to get frequencies/ counts for subsets.
+		-d	: Output frequencies/ counts of diplotypes
+		-c	: Path to a tab-delimited table for coding haplotypes
+		-h	: Displays help menu
 		""")
 		print()
 		sys.exit()
